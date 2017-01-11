e724781e27665be809389d1e9d3e1fd2dab30f26
FildasPA <julien.boge@alumni.univ-avignon.fr>
2017-01-11 22:11:57 +0100

    Update sfml.cpp & add font

--- src/main/sfml.cpp
+++ src/main/sfml.cpp
@@ -2,7 +2,7 @@
 //
 // ■ Main
 // -- Objet :
-// -- Par : Julien Boge
+// -- Par :
 // -- Dernière modification :
 //
 //=============================================================================
@@ -13,50 +13,108 @@
 #include <fstream>
 #include <sstream>
 #include <SFML/Graphics.hpp>
+#include "../lib/colors.cpp"
 #include "../lib/button.h"
 
 using namespace std;
+using namespace sf;
 
 //=============================================================================
-// ▼ Constantes
+// ▼ Variables globales
+// ----------------------------------------------------------------------------
+//
 //=============================================================================
-namespace files {
-	std::string save_directory = "save";
-	std::string user_infos = save_directory + "/user.dat";
+
+// Window
+RenderWindow window;
+
+// Sprites
+namespace sprite {
+	Sprite background;
 }
 
-// Colors
-#define RESET       "\033[0m"              // Reset
-#define BLACK       "\033[30m"             // Black
-#define RED         "\033[31m"             // Red
-#define GREEN       "\033[32m"             // Green
-#define YELLOW      "\033[33m"             // Yellow
-#define BLUE        "\033[34m"             // Blue
-#define MAGENTA     "\033[35m"             // Magenta
-#define CYAN        "\033[36m"             // Cyan
-#define WHITE       "\033[37m"             // White
-#define BOLDBLACK   "\033[1m\033[30m"      // Bold Black
-#define BOLDRED     "\033[1m\033[31m"      // Bold Red
-#define BOLDGREEN   "\033[1m\033[32m"      // Bold Green
-#define BOLDYELLOW  "\033[1m\033[33m"      // Bold Yellow
-#define BOLDBLUE    "\033[1m\033[34m"      // Bold Blue
-#define BOLDMAGENTA "\033[1m\033[35m"      // Bold Magenta
-#define BOLDCYAN    "\033[1m\033[36m"      // Bold Cyan
-#define BOLDWHITE   "\033[1m\033[37m"      // Bold White
+// Textures
+namespace texture {
+	Texture background;
+	Texture pause;
+	Texture resume;
+	Texture setting;
+	Texture reset;
+}
 
-/*
 //=============================================================================
-// ▼ Prototypes de fonctions
+// ▼ Window
+// ----------------------------------------------------------------------------
+//
 //=============================================================================
-void start();
-void post_start();
-void update();
-void pre_terminate();
-void terminate();
 
-void get_user_infos();
+//-----------------------------------------------------------------------------
+// * Create window
+//-----------------------------------------------------------------------------
+void createWindow()
+{
+	window.create(VideoMode(1024, 760),"Bobby va bientôt emmenager ici!");
+	window.setVerticalSyncEnabled(true);
+}
 
-*/
+//=============================================================================
+// ▼ Textures
+// ----------------------------------------------------------------------------
+//
+//=============================================================================
+
+//-----------------------------------------------------------------------------
+// * Load texture
+//-----------------------------------------------------------------------------
+bool loadTexture(Texture& texture, const char *fileName)
+{
+	if(!texture.loadFromFile(fileName)) {
+		std::cerr << "Erreur chargement de la texture " << fileName << std::endl;
+		return false;
+	}
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+// * Chargement des textures
+//-----------------------------------------------------------------------------
+void loadTextures()
+{
+	loadTexture(texture::background,"images/lac.jpg");
+	loadTexture(texture::pause,     "images/Background_medium.jpg");
+	loadTexture(texture::reset,     "images/reset.png");
+}
+
+
+//=============================================================================
+// ▼ Sprites
+// ----------------------------------------------------------------------------
+//
+//=============================================================================
+
+//-----------------------------------------------------------------------------
+// * Create background
+//-----------------------------------------------------------------------------
+void setSprites()
+{
+	sprite::background.setTexture(texture::background);
+}
+
+
+//=============================================================================
+// ▼ Update sprites
+// ----------------------------------------------------------------------------
+//
+//=============================================================================
+
+//-----------------------------------------------------------------------------
+// * Update background sprite
+//-----------------------------------------------------------------------------
+void updateBackground(Texture& texture)
+{
+	sprite::background.setTexture(texture);
+}
+
 //=============================================================================
 // ▼ Main
 // ----------------------------------------------------------------------------
@@ -64,159 +122,13 @@ void get_user_infos();
 //=============================================================================
 
 //-----------------------------------------------------------------------------
-// * Main
+// * Initialisation
 //-----------------------------------------------------------------------------
-int main()
+void init()
 {
-	// start();
-	// post_start();
-	// update();
-	// pre_terminate();
-	// terminate();
-
-	sf::Texture textureBackground;
-	sf::Texture texturePause;
-	sf::Texture textureResume;
-	sf::Texture textureSetting;
-	sf::Texture textureReset;
-
-
-	if (!textureBackground.loadFromFile("images/lac.jpg"))
-	{
-    		std::cout<<" Erreur chargement de la texture! "<<std::endl;
-		return 1;
-	}
-
-	if (!texturePause.loadFromFile("images/Background_medium.jpg"))
-				{
-    					std::cout<<" Erreur chargement de la texture! "<<std::endl;
-					return 1;
-				}
-/*
-	if (!textureResume.loadFromFile("images/lac.jpg"))
-	{
-    		std::cout<<" Erreur chargement de la texture! "<<std::endl;
-		return 1;
-	}
-
-	if (!textureSetting.loadFromFile("images/lac.jpg"))
-	{
-    		std::cout<<" Erreur chargement de la texture! "<<std::endl;
-		return 1;
-	}
-*/
-	if (!textureReset.loadFromFile("images/reset.png"))
-	{
-    		std::cout<<" Erreur chargement de la texture! "<<std::endl;
-		return 1;
-	}
-
-
-	sf::RenderWindow window(sf::VideoMode(1024, 760), "Nom de la fenêtre");
-	window.setVerticalSyncEnabled(true);
-
-	string State = "";
-
-	sf::Sprite backGround(textureBackground);
-	sf::Sprite pause(texturePause);
-	//sf::Sprite menuResume(textureResume);
-	//sf::Sprite menuSetting(textureSetting);
-	sf::Sprite menuReset(textureReset);
-
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-			{
-				window.close();
-			}
-			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
-			{
-
-				cout<<"Hello"<<endl;
-				window.draw(pause);
-/*
-				if (!texture.loadFromFile("images/Resume.jpg"))
-				{
-    					std::cout<<" Erreur chargement de la texture! "<<std::endl;
-					return 1;
-				}
-				menuResume.setTexture(texture);
-
-				if (!texture.loadFromFile("images/Setting.jpg"))
-				{
-    					std::cout<<" Erreur chargement de la texture! "<<std::endl;
-					return 1;
-				}
-				menuSetting.setTexture(texture);
-*/
-				//menuReset.setPosition(100,100);
-				window.draw(menuReset);	// voir si pas de conflit avec window.clear plus bas
-			}
-		}
-		window.clear();
-		window.draw(backGround);
-/*		
-		if(State == "Menu1")
-		{
-			if (!texture.loadFromFile("images/Menu1.jpg"))
-			{
-    				std::cout<<" Erreur chargement de la texture! "<<std::endl;
-				return 1;
-			}
-			sprite.setTexture(texture);
-
-			window.draw(sprite);
-			window.display();
-		}
-
-		if(State == "Menu2")
-		{
-			if (!texture.loadFromFile("images/Menu2.jpg"))
-			{
-    				std::cout<<" Erreur chargement de la texture! "<<std::endl;
-				return 1;
-			}
-			sprite.setTexture(texture);
-
-			window.draw(sprite);
-			window.display();
-		}
-
-		if(State == "Menu3")
-		{
-			if (!texture.loadFromFile("iamges/Menu3.jpg"))
-			{
-    				std::cout<<" Erreur chargement de la texture! "<<std::endl;
-				return 1;
-			}
-			sprite.setTexture(texture);
-
-			window.draw(sprite);
-			window.display();
-		}
-*/		
-	}
-	return 0;
-}
-/*
-//-----------------------------------------------------------------------------
-// * Start
-//-----------------------------------------------------------------------------
-void start()
-{
-	get_user_infos();
-	// get_pet_infos();
-}
-
-//-----------------------------------------------------------------------------
-// * Post-start
-//-----------------------------------------------------------------------------
-void post_start()
-{
-
+	loadTextures();
+	setSprites();
+	createWindow();
 }
 
 //-----------------------------------------------------------------------------
@@ -224,71 +136,115 @@ void post_start()
 //-----------------------------------------------------------------------------
 void update()
 {
+	while (window.isOpen())	{
+		Event event;
+		while (window.pollEvent(event)) {
+			// Fermeture de la fenêtre
+			if(event.type == Event::Closed) {
+				window.close();
+				return;
+			}
 
-}
+			//
+			// if((event.type == Event::KeyPressed) && (event.key.code == Keyboard::Escape)) {
+			    // std::cout << "Pause!" << std::endl;
+			// }
 
-//-----------------------------------------------------------------------------
-// * Pre-terminate
-//-----------------------------------------------------------------------------
-void pre_terminate()
-{
+			if (event.type == Event::TextEntered) {
+				if (event.text.unicode < 128)
+					std::cout << "tapé: " << static_cast<char>(event.text.unicode) << std::endl;
+			}
+		}
 
+		window.clear();
+		window.draw(sprite::background);
+		// window.draw(text);
+		// window.draw(shape);
+		window.display();
+	}
 }
 
 //-----------------------------------------------------------------------------
 // * Terminate
 //-----------------------------------------------------------------------------
-void terminate()
+void end()
 {
 
 }
 
-
-//=============================================================================
-// ▼ Category
-// ----------------------------------------------------------------------------
-// Description
-//=============================================================================
-
-
 //-----------------------------------------------------------------------------
-// * Get user informations
-// If can't find user file, create a new one.
+// * Main
 //-----------------------------------------------------------------------------
-void get_user_infos()
+int main()
 {
-	std::ofstream file;
-	file.open(files::user_infos.c_str()));
-	if(file.is_open()) {
-		file << pid;
-	} else {
-		std::cerr << "Erreur: impossible d'ouvrir le fichier ";
-		std::cerr << files::user_infos.c_str() << std::endl;
-	}
-	file.close();
+    init();
+    update();
+    end();
+
+    return 0;
 }
+                // window.draw(pause);
+/*
+                if (!texture.loadFromFile("images/Resume.jpg"))
+                {
+                        std::cout<<" Erreur chargement de la texture! "<<std::endl;
+                    return 1;
+                }
+                menuResume.setTexture(texture);
 
-
-//-----------------------------------------------------------------------------
-// * Write a string into a file
-//-----------------------------------------------------------------------------
-string read(const char* file_name,const char* text)
-{
-	std::string line;
-
-	std::ifstream file;
-	file.open(file_name));
-
-	if(file.is_open()) {
-		getline(file,line);
-		std::istringstream iss(line);
-	} else {
-		std::cerr << "Erreur: impossible d'ouvrir le fichier ";
-		std::cerr << file_name << std::endl;
-	}
-
-	file.close();
-
-	return line;
-}
+                if (!texture.loadFromFile("images/Setting.jpg"))
+                {
+                        std::cout<<" Erreur chargement de la texture! "<<std::endl;
+                    return 1;
+                }
+                menuSetting.setTexture(texture);
 */
+                //menuReset.setPosition(100,100);
+                // window.draw(menuReset);  // voir si pas de conflit avec window.clear plus bas
+            // }
+        // }
+        // window.clear();
+        // window.draw(backGround);
+/*
+        if(State == "Menu1")
+        {
+            if (!texture.loadFromFile("images/Menu1.jpg"))
+            {
+                    std::cout<<" Erreur chargement de la texture! "<<std::endl;
+                return 1;
+            }
+            sprite.setTexture(texture);
+
+            window.draw(sprite);
+            window.display();
+        }
+
+        if(State == "Menu2")
+        {
+            if (!texture.loadFromFile("images/Menu2.jpg"))
+            {
+                    std::cout<<" Erreur chargement de la texture! "<<std::endl;
+                return 1;
+            }
+            sprite.setTexture(texture);
+
+            window.draw(sprite);
+            window.display();
+        }
+
+        if(State == "Menu3")
+        {
+            if (!texture.loadFromFile("iamges/Menu3.jpg"))
+            {
+                    std::cout<<" Erreur chargement de la texture! "<<std::endl;
+                return 1;
+            }
+            sprite.setTexture(texture);
+
+            window.draw(sprite);
+            window.display();
+        }
+*/
+    // }
+    // return 0;
+// }
